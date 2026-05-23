1f1b83506bc358ae3906f44a07656508fffda06b
Erick Moura <erick.andrew.moura@gmail.com>
2026-05-23 01:55:33 -0300

    "Adding Views"

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
