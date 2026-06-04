82ca3eed363654157c037608a7785b4ac28d903e
Erick Moura <erick.andrew.moura@gmail.com>
2026-06-04 13:46:16 -0300

    "Admin Dashboard and some admin updates"

--- utilities/index.js
+++ utilities/index.js
@@ -2,17 +2,19 @@ require("dotenv").config()
 const Util = {}
 
 Util.getUsersGrid = async function(data) {
-    let grid = "";
-    if (data && data.length > 0) {
-      grid = '<ul id="inv-display">';
-      data.forEach(user => {
-        grid += `<li> ${user.userName}, ${user.userEmail}, ${user.userPassword}, ${user.userRole}, ${user.userId} </li> 
-        <a href="/admin/users/${user.userId}">Edit</a>`;
-    });
-    grid += "</ul>";
+  let grid = "";
+  grid = '<a href="/admin/users/create">Create User</a>';
+  if (data && data.length > 0) {
+    grid += '<ul id="inv-display">';
+    data.forEach(user => {
+      grid += `<li> ${user.userName}, ${user.userEmail}, ${user.userPassword}, ${user.userRole}, ${user.userId} </li> 
+      <a href="/admin/users/${user.userId}">Edit</a>`;
+  });
+  grid += "</ul>";
   } else { 
-    grid = '<p class="notice">Sorry, no users could be found.</p>';
+    grid += '<p class="notice">Sorry, no users could be found.</p>';
   }
+  grid += '<a href="/admin">Return</a>';
   return grid;
 }
 
@@ -33,18 +35,19 @@ Util.getVehiclesGrid = async function (data) {
 
 Util.admGetVehiclesGrid = async function (data) {
   let grid = "";
+  grid = '<a href="/admin/vehicles/create">Create Car</a>';
   if (data && data.length > 0){
-    grid = '<ul id="inv-display">';
+    grid += '<ul id="inv-display">';
     data.forEach(vehicle=>{
       grid += `<li>vehicle: ${vehicle.vehicleId} ${vehicle.brand} ${vehicle.model} ${vehicle.year} ${vehicle.dailyPrice} </li>
       <a href="/admin/vehicles/${vehicle.vehicleId}">Edit</a>`
     })
     grid += "</ul>";
+  } else {
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>';
   }
-    else {
-      grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
-    }
-    return grid;
+  grid += '<a href="/admin">Return</a>';
+  return grid;
 }
 
 Util.getVehiclesGridId = async function (data) {
