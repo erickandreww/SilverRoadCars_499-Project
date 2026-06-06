309cb09b5f816607870cb17673369c90b2a06dc5
rawinolivera <oli18001@byui.edu>
2026-06-06 20:46:23 -0300

    Web site definition images and pictures

--- public/scripts/base.js
+++ public/scripts/base.js
@@ -1,3 +1,5 @@
+//Nav Menu
+
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector("ul");
 const span = document.querySelector("#top");
@@ -16,3 +18,31 @@ window.addEventListener("resize", () => {
     span.classList.remove("open");
   }
 });
+
+//Carousel 
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showSlide(index) {
+  slides[current].classList.remove("active");
+
+  current = index;
+
+  if (current >= slides.length) current = 0;
+  if (current < 0) current = slides.length - 1;
+
+  slides[current].classList.add("active");
+}
+
+document.getElementById("next").addEventListener("click", () => {
+  showSlide(current - 1);
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+  showSlide(current - 1);
+});
+
+setInterval(() => {
+  showSlide(current + 1);
+}, 7000);
