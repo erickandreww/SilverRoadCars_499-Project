05b616bfc7f687d03016f85228942d8dc86a72bc
Erick Moura <erick.andrew.moura@gmail.com>
2026-06-09 18:48:19 -0300

    "Adding Maintenance function and pages"

---
+++ models/maintenance.js
@@ -0,0 +1,70 @@
+const pool = require("../config/db")
+const crypto = require("crypto");
+
+async function getAllMaintenances() {
+  const sql = `
+    SELECT m.*, v."brand", v."model", v."plate", u."userName" AS "createdByName"
+    FROM "Maintenance" m
+    LEFT JOIN "Vehicles" v ON m."vehicleId" = v."vehicleId"
+    LEFT JOIN "Users" u ON m."createdBy" = u."userId"
+    ORDER BY m."createdAt" DESC
+  `;
+
+  const result = await pool.query(sql);
+  return result.rows;
+}
+
+async function getMaintenance(maintenanceId) {
+  const sql = `SELECT * FROM "Maintenance" WHERE "maintenanceId" = $1`;
+  const result = await pool.query(sql, [maintenanceId]);
+  return result.rows[0];
+}
+
+async function newMaintenance(vehicleId, createdBy, maintenanceType, 
+  description, maintenanceDate, cost, status
+) {
+  const maintenanceId = crypto.randomUUID();
+
+  const sql = `
+    INSERT INTO "Maintenance" ("maintenanceId", "vehicleId", "createdBy", "maintenanceType", 
+    "description", "maintenanceDate", "cost", "status", "createdAt", "updatedAt")
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, NOW(), NOW())
+    RETURNING *;
+    `;
+
+  const result = await pool.query(sql, [maintenanceId, vehicleId, createdBy, 
+    maintenanceType, description, maintenanceDate, cost, status]);
+
+  return result.rows[0];
+}
+
+async function updateMaintenance(maintenanceId, vehicleId, maintenanceType, 
+  description, maintenanceDate, cost, status
+) {
+  const sql = `
+    UPDATE "Maintenance"
+    SET
+      "vehicleId" = $1, "maintenanceType" = $2, "description" = $3, 
+      "maintenanceDate" = $4, "cost" = $5, "status" = $6, 
+      "updatedAt" = NOW()
+    WHERE "maintenanceId" = $7
+    RETURNING *;
+  `;
+
+  const result = await pool.query(sql, [
+    vehicleId, maintenanceType, description, 
+    maintenanceDate, cost, status, maintenanceId
+  ]);
+
+  return result.rows[0];
+}
+
+async function deleteMaintenance(maintenanceId) {
+  const sql = `DELETE FROM "Maintenance" WHERE "maintenanceId" = $1 RETURNING *;`;
+  
+  const result = await pool.query(sql, [maintenanceId]);
+
+  return result.rows[0];
+}
+
+module.exports = { getAllMaintenances, getMaintenance , newMaintenance, updateMaintenance, deleteMaintenance}
